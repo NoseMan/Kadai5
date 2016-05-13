833bda275627eadf706482f032f16b62f2a84d11
g13943se <g13943se@s30453.tsuda.ac.jp>
2016-05-13 11:49:45 +0900

    どらいばーしむしむ＾＾ なびげーたーしまじろう＾＾
    1桁目の処理終了(テスト未)
    2桁目のメソッド外形のみ作成(参照用配列未作成、10~19のメソッドと参照用配列も未作成)

--- src/IntToEng.java
+++ src/IntToEng.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class IntToEng {
+	
+	static final String[] oneToNine = {"one", "two", "three", "four", "five", "six", "seven"
+			, "eight", "nine"};
+	static final String[] twentyToNinety = {"twenty"}; // yoro
+	
     // メインメソッド
     public static void main(String[] args) {
 
@@ -13,14 +18,23 @@ public class IntToEng {
 
     // 数値を英訳する変換するメソッド
     static String translateEng(int n) {
-    	String eng;
-    	int len = String.valueOf(n).length();
-    	int d = (int)Math.pow(10, len-1);
-    	if(n == 0){
-    		return "zero";
+    	String result = "";
+    	
+    	if(n==0) return "zero"; // 0の場合は例外としてzeroを返す
+    	char[] nchar = String.valueOf(n).toCharArray(); // nをstringに変換後char[]に分割
+    	for(int i=0;i>(nchar.length/3);i++){
+    		for(int j=2;j>=0;j--){
+    			if(j==1 && (int)nchar[1+i]!=1) result += translate1to9((int)nchar[j+i]); // 1桁め
+    			if(j==2){ // 2桁め
+    				result += ((int)nchar[j+i]!=1 ? /*10~19の場合のメソッド*/ : /*通常のメソッド*/);
+    			}
+    			if(j==3); // 3桁め
+    		}
     	}
     	
-    	}
-        return eng;
+        return result;
     }
+    
+    static String translate1to9(int one){ return oneToNine[one-1]; }
+    static String translate20to90(int ten){ return twentyToNinety[ten-1]; }
 }
