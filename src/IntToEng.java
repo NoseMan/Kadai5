917f5a1583b4f3a706cb192eab9aa5f01859fee6
g13942sy <g13942sy@m31302.tsuda.ac.jp>
2016-05-18 17:27:43 +0900

    入力した数の要素数を0を用いて3の倍数に調整したのち、char配列に逆順に入れました。 　　これで3で割り切れない数が入力されても対応できるようになりました。 テストを作成しました。 　　0,一桁の数,10~19の数を入れた時のテストケースを作成しました。 　　0,一桁の数のテストは成功しましたが、10~19の数のテストは失敗しました。 　　translateEngの出力結果に空白が含まれているからと考えられるので、そこを修正してください。
    ナビゲーター　しむしむ
    ドライバー　しまじろう

--- src/IntToEng.java
+++ src/IntToEng.java
@@ -8,6 +8,7 @@ public class IntToEng {
 			,"eighty","ninety"}; 
 	static final String[] tenToNineteen = {"ten","eleven","twelve","thirteen","fourteen","fifteen"
 			,"sixteen","seventeen","eighteen","nineteen"};
+	static final String[] largeUnit = {"thousand","million","billion","trillion","quadrillion"};
     // メインメソッド
     public static void main(String[] args) {
 
@@ -21,34 +22,40 @@ public class IntToEng {
     // 数値を英訳する変換するメソッド
     static String translateEng(int n) {
     	StringBuilder result = new StringBuilder();
-    	
-    	if(n==0) return "zero"; // 0の場合は例外としてzeroを返す
-    	char[] nchar = String.valueOf(n).toCharArray(); // nをstringに変換後char[]に分割
-    	for(int i=0;i<nchar.length;i++) System.out.print(nchar[i]+" "); // debug
+     	if(n==0) return "zero"; // 0の場合は例外としてzeroを返す
+    	String s = String.valueOf(n);//nをStringに変換後桁数を3の倍数に補正
+    	while(s.length() % 3 != 0){
+    		s = "0"+s;
+    	}
+    	char[] nchar = s.toCharArray(); // sをchar[]に分割
+    	for(int i = 0 ; i <= (nchar.length/2)-1 ; i++){//入れ替え
+    		char a = nchar[i];
+    		nchar[i] = nchar[nchar.length-1-i];
+    		nchar[nchar.length-1-i] = a;
+    	}
+    	//for(int i=0;i<nchar.length;i++) System.out.print(nchar[i]+" "); // debug
     	boolean isZero = true; //全部0か？
     	for(int i=(nchar.length/3)-1;i>=0;i--){
-    		System.out.println("iは" + i);
+    		//System.out.println("iは" + i); // debug
     		for(int j=2;j>=0;j--){
-    			System.out.println("jは" + j);
+    			//System.out.println("jは" + j); //debug
     			int charNum = Character.getNumericValue(nchar[j+(i*3)]);
     			if(charNum != 0){
-    				System.out.println("isZeroはおっけー nchar = "+ charNum);
+    				//System.out.println("isZeroはおっけー nchar = "+ charNum); //debug
     				isZero = false;
-    				if(j==0 && Character.getNumericValue(nchar[1+(i*3)])!=1 ) result.append(translate1to9(charNum)); // 1桁め
+    				if(j==0 && Character.getNumericValue(nchar[1+(i*3)])!=1 ) result.append(oneToNine[charNum-1] + " "); // 1桁め
     				if(j==1){ // 2桁め
-    					result.append(charNum==1 ? translate10to19(Character.getNumericValue(nchar[i*3])) : translate20to90(charNum));//2桁め
+    					result.append(charNum==1 ? tenToNineteen[Character.getNumericValue(nchar[i*3])] + " " : twentyToNinety[charNum-2]+" ");//2桁め
     				}
     				if(j==2){
-    					result.append(translate1to9(charNum) +  "hundred");// 3桁め
+    					result.append(oneToNine[charNum-1] + " " +  "hundred " );// 3桁め
     				}	
     			}
     		}
+    		if(!isZero && i != 0) result.append(largeUnit[i-1] + " ");
     	}
     	
         return result.toString();
     }
     
-    static String translate1to9(int n){ System.out.println("translate1to9 throw n="+n); return oneToNine[n-1]; }
-    static String translate20to90(int n){ System.out.println("translate20to90 throw n="+n); return twentyToNinety[n-1]; }
-    static String translate10to19(int n){ System.out.println("translate10to19 throw n="+n); return tenToNineteen[n]; }
 }
