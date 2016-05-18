330ceca44c6d22a1d6540d02b04b92ad4f9a100b
g13942sy <g13942sy@m31302.tsuda.ac.jp>
2016-05-18 15:10:02 +0900

    2・3桁目の処理は終わったが、入力が一桁だけの際(nchar[1]やnchar[2]が存在しない時)にエラー
    ドライバーしまじろう＾＾
    ナビゲーターしむしむ＾＾

--- src/IntToEng.java
+++ src/IntToEng.java
@@ -4,8 +4,10 @@ public class IntToEng {
 	
 	static final String[] oneToNine = {"one", "two", "three", "four", "five", "six", "seven"
 			, "eight", "nine"};
-	static final String[] twentyToNinety = {"twenty"}; // yoro
-	
+	static final String[] twentyToNinety = {"twenty","thirty","fourty","fifty","sixty","seventy"
+			,"eighty","ninety"}; 
+	static final String[] tenToNineteen = {"ten","eleven","twelve","thirteen","fourteen","fifteen"
+			,"sixteen","seventeen","eighteen","nineteen"};
     // メインメソッド
     public static void main(String[] args) {
 
@@ -22,19 +24,29 @@ public class IntToEng {
     	
     	if(n==0) return "zero"; // 0の場合は例外としてzeroを返す
     	char[] nchar = String.valueOf(n).toCharArray(); // nをstringに変換後char[]に分割
-    	for(int i=0;i>(nchar.length/3);i++){
+    	boolean isZero = true; //全部0か？
+    	for(int i=(nchar.length/3);i>=0;i--){
+    		System.out.println("iは" + i);
     		for(int j=2;j>=0;j--){
-    			if(j==1 && (int)nchar[1+i]!=1) result += translate1to9((int)nchar[j+i]); // 1桁め
-    			if(j==2){ // 2桁め
-    				result += ((int)nchar[j+i]!=1 ? /*10~19の場合のメソッド*/ : /*通常のメソッド*/);
+    			System.out.println("jは" + j);
+    			if((int)nchar[j+(i*3)] != 0){
+    				System.out.println("isZeroはおっけー nchar = "+ nchar[j+(i*3)]);
+    				isZero = false;
+    				if(j==0 && (int)nchar[1+(i*3)]!=1 ) result += translate1to9((int)nchar[(i*3)]); // 1桁め
+    				if(j==1){ // 2桁め
+    					result += ((int)nchar[1+(i*3)]==1 ? translate10to19((int)nchar[i*3]) : translate20to90((int)nchar[1+(i*3)]));//2桁め
+    				}
+    				if(j==2){
+    					result +=  translate1to9((int)nchar[2+(i*3)]) +  "hundred";// 3桁め
+    				}	
     			}
-    			if(j==3); // 3桁め
     		}
     	}
     	
         return result;
     }
     
-    static String translate1to9(int one){ return oneToNine[one-1]; }
-    static String translate20to90(int ten){ return twentyToNinety[ten-1]; }
+    static String translate1to9(int n){ return oneToNine[n-1]; }
+    static String translate20to90(int n){ return twentyToNinety[n-1]; }
+    static String translate10to19(int n){ return tenToNineteen[n]; }
 }
